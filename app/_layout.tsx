626bb3962baec05a1f66556dd4aa9ccd4967d6b8
Daniel Chanda <danielchanda41@gmail.com>
2025-02-02 15:51:03 +0200

    Tailwind config fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,10 +28,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="profile" options={{ headerShown: false }}/>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
